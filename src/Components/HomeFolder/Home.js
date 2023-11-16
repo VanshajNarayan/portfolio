a48836db361ff4f1d8597db86a66e88c7ec208c6
VanshajNarayan <vihaanp748@gmail.com>
2023-11-16 23:01:00 +0530

    add: addhomesection

--- src/Components/HomeFolder/Home.js
+++ src/Components/HomeFolder/Home.js
@@ -1,11 +1,50 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import "./Home.css";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
+  const text = useTypewriter({
+    words: ["Frontend developer", "React developer"],
+    loop: {},
+    typeSpeed: 120,
+    delaySpeed: 50,
+  });
   return (
     <>
-      <h3>This is Home page</h3>
+      <section className="heroSection">
+        <div className="heroBox">
+          <div className="imgBox">
+            <div className="imgborder"></div>
+          </div>
+          <div className="textBox">
+            <p
+              style={{
+                color: "#fff",
+                fontSize: "1.2rem",
+                textTransform: "uppercase",
+              }}>
+              I'm
+            </p>
+            <p className="vanshaj">Vanshaj</p>
+            <p className="Narayan">Narayan</p>
+            <p
+              style={{
+                color: "#fff",
+                fontSize: "2.3rem",
+                textTransform: "uppercase",
+                letterSpacing: "0.2rem",
+                fontWeight: "600",
+              }}>
+              {text[0]} <Cursor cursorStyle="|" cursorColor="red" />
+            </p>
+            <NavLink to="/Contact">
+              <button className="btn">Contact</button>
+            </NavLink>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 };
 
 export default Home;
